9b8e6ae12c27c449af6e421546c6995e4d8fc49a
Esteban <juanescalderon12@gmail.com>
2023-02-22 11:00:33 -0500

    feat: Counter básico
    Tenemos nuevos métodos

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,14 +1,50 @@
 <template>
-  <h2>Counter</h2>
-  <p>10</p>
+  <h2>Counter!!!</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareCounter }}</p>
+
+  <div>
+    <button @click="increase()">+ 1</button>
+    <button @click="decrease()">- 1</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Pato'
-}
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  methods: {
+    increase() {
+      this.counter++;
+    },
+    decrease() {
+      this.counter--;
+    },
+  },
+  computed: {
+    getSquareCounter() {
+      return this.counter * this.counter;
+    },
+  },
+};
 </script>
 
 <style>
+button {
+  background-color: #64bb87;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
 
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3 ease-in-out;
+}
 </style>
